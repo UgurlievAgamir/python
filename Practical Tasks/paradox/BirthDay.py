ce67d2f91729e33f132c9fc08e6e491418cb3926
UgurlievAgamir <ugurlievam@gmail.com>
2022-10-25 15:47:14 +0300

    Update 2 practical task

--- Practical Tasks/paradox/BirthDay.py
+++ Practical Tasks/paradox/BirthDay.py
@@ -1,31 +1,32 @@
 import random
 
 
-def birthday(people_count):
+def birthday(people_count, iterations_count=1000):
     days = [day for day in range(0, 29)]
     months = [month for month in range(0, 13)]
-
-    D_data = []
-    M_data = []
 
     count_coincidences = 0
     count_not_coincidences = 0
 
-    for i in range(0, people_count + 1):
-        D_data.append(random.choice(days))
-        M_data.append(random.choice(months))
+    for iterations in range(0, iterations_count + 1):
+        D_data = []
+        M_data = []
 
-    for i in range(0, people_count):
-        if D_data.count(D_data[i]) == 1 or M_data.count(M_data[i]) == 1:
-            count_not_coincidences += 1
-            pass
+        for i in range(0, people_count + 1):
+            D_data.append(random.choice(days))
+            M_data.append(random.choice(months))
 
-        for j in range(0, people_count):
-            if j == i:
+        for i in range(0, people_count):
+            if D_data.count(D_data[i]) == 1 or M_data.count(M_data[i]) == 1:
+                count_not_coincidences += 1
                 pass
 
-            if D_data[j] == D_data[i] and M_data[j] == M_data[i]:
-                count_coincidences += 1
+            for j in range(0, people_count):
+                if j == i:
+                    pass
+
+                if D_data[j] == D_data[i] and M_data[j] == M_data[i]:
+                    count_coincidences += 1
 
     return f"количество совпадений: {count_coincidences} " \
            f"количество не совпадений: {count_not_coincidences} " \
--- Practical Tasks/testForParadox.py
+++ Practical Tasks/testForParadox.py
@@ -1,4 +1,4 @@
 from paradox import *
 
-print(birthday(24))
+print(birthday(24, 10000))
 print(MontyHall(23123))
