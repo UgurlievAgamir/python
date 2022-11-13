74146f83dd162450d4369320dc4923e5c46dd476
The Fire Owl <61467254+Owl-Man@users.noreply.github.com>
2022-11-13 14:52:35 +0300

    Completed practice 4

--- PracticalTasks/Practice_4/file_handler.py
+++ PracticalTasks/Practice_4/file_handler.py
@@ -1,19 +1,24 @@
 def read_file(file_name):
     output_text = {}
     with open(file_name, encoding='UTF-8') as file:
-        cache_list = file.read().split(' ')
-
-    for i in range(0, len(cache_list)):
-        cache_list[i] = cache_list[i].lower()
-
-    output_text = set(cache_list)
-
-    return output_text
-
-
-def write_file(file_name):
-    with open(file_name, encoding='UTF-8', mode='r+') as file:
         words_list = file.read().split(' ')
 
-        for i in range(0, len(words_list)):
-            words_list[i]
+    for i in range(0, len(words_list)):
+        words_list[i] = words_list[i].lower()
+        words_list[i] = "".join(a for a in words_list[i] if a.isalpha())
+
+    for word in words_list:
+        if word == '' or len(word) == 1:
+            words_list.remove(word)
+
+    output_text = set(words_list)
+
+    return list(output_text)
+
+
+def write_file(file_name, words_list):
+    words_list = sorted(words_list)
+    with open(file_name, encoding='UTF-8', mode='w') as file:
+        file.write('Всего уникальных слов: ' + str(len(words_list)))
+        file.write('\n======================\n')
+        file.write('\n'.join(words_list))
--- PracticalTasks/Practice_4/test.py
+++ PracticalTasks/Practice_4/test.py
@@ -1,3 +1,4 @@
 import file_handler
 
 print(file_handler.read_file('read_file'))
+file_handler.write_file('save_file', file_handler.read_file('read_file'))
