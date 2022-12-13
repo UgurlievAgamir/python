e9d4db6e8ca2530cb0d00f797f9fe6c72b2bf21e
UgurlievAgamir <ugurlievam@gmail.com>
2022-12-13 15:50:56 +0300

    practice_8

---
+++ PracticalTasks/Practice_8/english_learn.py
@@ -0,0 +1,48 @@
+import operator
+from typing import Any, Dict
+
+import pymorphy2
+import translate
+
+phrasebook = {}
+
+morph = pymorphy2.MorphAnalyzer()
+en_translator = translate.Translator(from_lang='ru', to_lang='en')
+
+
+def get_english_phrasebook(file_name: str) -> None:
+    with open(file_name, encoding='windows-1251') as file:
+        list = file.read().split()
+        phrasebook = get_sorted_dict(normalize_words_list(list))
+        write_dict_to_file('phrasebook.txt')
+
+
+def write_dict_to_file(file_name: str) -> None:
+    with open(file_name, encoding='windows-1251', mode='w') as file:
+        for word in phrasebook:
+            print(word)
+            file.write(f'{word} | {en_translator.translate(word)} | {phrasebook[word]}')
+
+
+def get_sorted_dict(words: list) -> Dict[Any, Any]:
+    output = {}
+
+    for word in words:
+        output[word] = str(words.count(word))
+
+    return dict(sorted(output.items(), key=operator.itemgetter(1), reverse=True))
+
+
+def normalize_words_list(words_list: list) -> list:
+    for i in range(0, len(words_list)):
+        words_list[i] = "".join(a for a in words_list[i] if a.isalpha())
+        words_list[i] = get_normal_form(words_list[i])
+
+    return words_list
+
+
+def get_normal_form(word: str) -> str:
+    return morph.parse(word)[0].normal_form
+
+
+get_english_phrasebook('data.txt')
